9719e8c108fb69bbfde2c9e4d0fe7d69c30e98ec
Dimitar Anguelov <113285865+dimitar9090@users.noreply.github.com>
2023-01-12 12:13:06 +0200

    Update decks.py

--- decks.py
+++ decks.py
@@ -10,3 +10,262 @@ def aurora_deck():
     frame.create_text(250, 200, text="4: Berserker's Helm")
     frame.create_text(250, 250, text="5: Mana Black Hole")
     frame.create_text(250, 300, text="6:  High-end Armor")
+
+def belica_deck():
+    clean_screen()
+    frame.create_text(250, 50, text="1: Witch's Vial")
+    frame.create_text(250, 100, text="2: Mathematician's Intro to Architecture")
+    frame.create_text(250, 150, text="3: Embodiment of Vengeance")
+    frame.create_text(250, 200, text="4: Fruit of Greed")
+    frame.create_text(250, 250, text="5: Gloves of Jealousy")
+    frame.create_text(250, 300, text="6: Merchant's Jewelry Box")
+
+
+def countes_deck():
+    clean_screen()
+    frame.create_text(250, 50, text="")
+    frame.create_text(250, 100, text="")
+    frame.create_text(250, 150, text="")
+    frame.create_text(250, 200, text="")
+    frame.create_text(250, 250, text="")
+    frame.create_text(250, 300, text="")
+
+
+def dekker_deck():
+    clean_screen()
+    frame.create_text(250, 50, text="")
+    frame.create_text(250, 100, text="")
+    frame.create_text(250, 150, text="")
+    frame.create_text(250, 200, text="")
+    frame.create_text(250, 250, text="")
+    frame.create_text(250, 300, text="")
+
+
+def feng_mao_deck():
+    clean_screen()
+    frame.create_text(250, 50, text="")
+    frame.create_text(250, 100, text="")
+    frame.create_text(250, 150, text="")
+    frame.create_text(250, 200, text="")
+    frame.create_text(250, 250, text="")
+    frame.create_text(250, 300, text="")
+
+
+def gadget_deck():
+    clean_screen()
+    frame.create_text(250, 50, text="")
+    frame.create_text(250, 100, text="")
+    frame.create_text(250, 150, text="")
+    frame.create_text(250, 200, text="")
+    frame.create_text(250, 250, text="")
+    frame.create_text(250, 300, text="")
+
+
+def gideo_deck():
+    clean_screen()
+    frame.create_text(250, 50, text="")
+    frame.create_text(250, 100, text="")
+    frame.create_text(250, 150, text="")
+    frame.create_text(250, 200, text="")
+    frame.create_text(250, 250, text="")
+    frame.create_text(250, 300, text="")
+
+
+def grux_deck():
+    clean_screen()
+    frame.create_text(250, 50, text="")
+    frame.create_text(250, 100, text="")
+    frame.create_text(250, 150, text="")
+    frame.create_text(250, 200, text="")
+    frame.create_text(250, 250, text="")
+    frame.create_text(250, 300, text="")
+
+
+def howitzer_deck():
+    clean_screen()
+    frame.create_text(250, 50, text="")
+    frame.create_text(250, 100, text="")
+    frame.create_text(250, 150, text="")
+    frame.create_text(250, 200, text="")
+    frame.create_text(250, 250, text="")
+    frame.create_text(250, 300, text="")
+
+
+def kallari_deck():
+    clean_screen()
+    frame.create_text(250, 50, text="")
+    frame.create_text(250, 100, text="")
+    frame.create_text(250, 150, text="")
+    frame.create_text(250, 200, text="")
+    frame.create_text(250, 250, text="")
+    frame.create_text(250, 300, text="")
+
+
+def khaimera_deck():
+    clean_screen()
+    frame.create_text(250, 50, text="")
+    frame.create_text(250, 100, text="")
+    frame.create_text(250, 150, text="")
+    frame.create_text(250, 200, text="")
+    frame.create_text(250, 250, text="")
+    frame.create_text(250, 300, text="")
+
+
+def kwang_deck():
+    clean_screen()
+    frame.create_text(250, 50, text="")
+    frame.create_text(250, 100, text="")
+    frame.create_text(250, 150, text="")
+    frame.create_text(250, 200, text="")
+    frame.create_text(250, 250, text="")
+    frame.create_text(250, 300, text="")
+
+
+def murdock_deck():
+    clean_screen()
+    frame.create_text(250, 50, text="")
+    frame.create_text(250, 100, text="")
+    frame.create_text(250, 150, text="")
+    frame.create_text(250, 200, text="")
+    frame.create_text(250, 250, text="")
+    frame.create_text(250, 300, text="")
+
+
+def muriel_deck():
+    clean_screen()
+    frame.create_text(250, 50, text="")
+    frame.create_text(250, 100, text="")
+    frame.create_text(250, 150, text="")
+    frame.create_text(250, 200, text="")
+    frame.create_text(250, 250, text="")
+    frame.create_text(250, 300, text="")
+
+
+def narbash_deck():
+    clean_screen()
+    frame.create_text(250, 50, text="")
+    frame.create_text(250, 100, text="")
+    frame.create_text(250, 150, text="")
+    frame.create_text(250, 200, text="")
+    frame.create_text(250, 250, text="")
+    frame.create_text(250, 300, text="")
+
+
+def phase_deck():
+    clean_screen()
+    frame.create_text(250, 50, text="")
+    frame.create_text(250, 100, text="")
+    frame.create_text(250, 150, text="")
+    frame.create_text(250, 200, text="")
+    frame.create_text(250, 250, text="")
+    frame.create_text(250, 300, text="")
+
+
+def rampage_deck():
+    clean_screen()
+    frame.create_text(250, 50, text="")
+    frame.create_text(250, 100, text="")
+    frame.create_text(250, 150, text="")
+    frame.create_text(250, 200, text="")
+    frame.create_text(250, 250, text="")
+    frame.create_text(250, 300, text="")
+
+
+def revenant_deck():
+    clean_screen()
+    frame.create_text(250, 50, text="")
+    frame.create_text(250, 100, text="")
+    frame.create_text(250, 150, text="")
+    frame.create_text(250, 200, text="")
+    frame.create_text(250, 250, text="")
+    frame.create_text(250, 300, text="")
+
+
+def serath_deck():
+    clean_screen()
+    frame.create_text(250, 50, text="")
+    frame.create_text(250, 100, text="")
+    frame.create_text(250, 150, text="")
+    frame.create_text(250, 200, text="")
+    frame.create_text(250, 250, text="")
+    frame.create_text(250, 300, text="")
+
+
+def severog_deck():
+    clean_screen()
+    frame.create_text(250, 50, text="")
+    frame.create_text(250, 100, text="")
+    frame.create_text(250, 150, text="")
+    frame.create_text(250, 200, text="")
+    frame.create_text(250, 250, text="")
+    frame.create_text(250, 300, text="")
+
+
+def shinbi_deck():
+    clean_screen()
+    frame.create_text(250, 50, text="")
+    frame.create_text(250, 100, text="")
+    frame.create_text(250, 150, text="")
+    frame.create_text(250, 200, text="")
+    frame.create_text(250, 250, text="")
+    frame.create_text(250, 300, text="")
+
+
+def sparrow_deck():
+    clean_screen()
+    frame.create_text(250, 50, text="")
+    frame.create_text(250, 100, text="")
+    frame.create_text(250, 150, text="")
+    frame.create_text(250, 200, text="")
+    frame.create_text(250, 250, text="")
+    frame.create_text(250, 300, text="")
+
+
+def steel_deck():
+    clean_screen()
+    frame.create_text(250, 50, text="")
+    frame.create_text(250, 100, text="")
+    frame.create_text(250, 150, text="")
+    frame.create_text(250, 200, text="")
+    frame.create_text(250, 250, text="")
+    frame.create_text(250, 300, text="")
+
+
+def the_fey_deck():
+    clean_screen()
+    frame.create_text(250, 50, text="")
+    frame.create_text(250, 100, text="")
+    frame.create_text(250, 150, text="")
+    frame.create_text(250, 200, text="")
+    frame.create_text(250, 250, text="")
+    frame.create_text(250, 300, text="")
+
+
+def twinblast_deck():
+    clean_screen()
+    frame.create_text(250, 50, text="")
+    frame.create_text(250, 100, text="")
+    frame.create_text(250, 150, text="")
+    frame.create_text(250, 200, text="")
+    frame.create_text(250, 250, text="")
+    frame.create_text(250, 300, text="")
+
+
+def wraith_deck():
+    clean_screen()
+    frame.create_text(250, 50, text="")
+    frame.create_text(250, 100, text="")
+    frame.create_text(250, 150, text="")
+    frame.create_text(250, 200, text="")
+    frame.create_text(250, 250, text="")
+    frame.create_text(250, 300, text="")
+
+
+def zena_deck():
+    clean_screen()
+    frame.create_text(250, 50, text="")
+    frame.create_text(250, 100, text="")
+    frame.create_text(250, 150, text="")
+    frame.create_text(250, 200, text="")
+    frame.create_text(250, 250, text="")
+    frame.create_text(250, 300, text="")
